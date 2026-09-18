1a3e81a4bdd481634221cfbd4758f55a15a6922c
Abdulrahman Sultan <119352671+abdlfc11@users.noreply.github.com>
2026-09-18 14:54:05 +0100

    chore: Add generic save route error message

--- static/js/utils/error-contants.js
+++ static/js/utils/error-contants.js
@@ -7,6 +7,8 @@ export const ERROR_MESSAGES = {
         OUTSIDE_CUMBRIA:
             "Please click on a point within Cumbria.",
         GENERIC_ROUTE_ACTION: // e.g undo route, redo route, clear path
-            "Sorry, there was an unexpected error performing that action, please try again later"
+            "Sorry, there was an unexpected error performing that action, please try again later",
+        GENERIC_SAVE_ROUTE:
+            "There was an unexpected error whilst saving your point, try again later."
     },
 };
